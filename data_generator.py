b835b2bb4f0e1abf78d7fa50d26a67e41a9b9277
Kapil Verma <99kapilverma@gmail.com>
2021-09-20 22:00:25 +0530

    Update data_generator.py

--- data_generator.py
+++ data_generator.py
@@ -2,9 +2,9 @@
 import os
 import numpy as np
 import utils
-import keras
+import tensorflow as tf
 
-class TrainDataGenerator(keras.utils.all_utils.Sequence):
+class TrainDataGenerator(tf.keras.utils.Sequence):
     '''inherits from Keras Sequence base object, allows to use multiprocessing in .fit_generator'''
     def __init__(self, samples, img_dir, batch_size, n_classes, basenet_preprocess, img_format,
                  img_load_dims=(256, 256), img_crop_dims=(224, 224), shuffle=True):
@@ -57,7 +57,7 @@ class TrainDataGenerator(keras.utils.all_utils.Sequence):
         return X, y
 
 
-class TestDataGenerator(keras.utils.all_utils.Sequence):
+class TestDataGenerator(tf.keras.utils.Sequence):
     '''inherits from Keras Sequence base object, allows to use multiprocessing in .fit_generator'''
     def __init__(self, samples, img_dir, batch_size, n_classes, basenet_preprocess, img_format,
                  img_load_dims=(224, 224)):
