b88e99dd189114527848cbba0720762b6fcb19ca
Kapil Verma <99kapilverma@gmail.com>
2024-04-09 09:55:23 +0200

    Update utils.py
    keras.preprocessing API is deprecated i

--- utils.py
+++ utils.py
@@ -36,7 +36,7 @@ def random_horizontal_flip(img):
 
 
 def load_image(img_file, target_size):
-    return np.asarray(keras.preprocessing.image.load_img(img_file, target_size=target_size))
+    return np.asarray(keras.utils.load_img(img_file, target_size=target_size))
 
 
 def normalize_labels(labels):
