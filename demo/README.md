70dddc021e3a6a5d6f6e161142d3f59023cb5fcb
Benjamin Bischoff <benjamin.bischoff@gmx.de>
2024-04-17 19:30:22 +0200

    new inject script and json source

---
+++ demo/README.md
@@ -0,0 +1,6 @@
+# Optilens
+
+## Demo
+
+- [Item search without Optilens](https://www.trivago.co.uk/en-GB/srl/hotel-nh-d%C3%BCsseldorf-city?search=100-6365;dr-20240426-20240427;rc-1-2-10)
+- [Item search without Optilens](https://www.trivago.co.uk/en-GB/srl/hotel-nh-d%C3%BCsseldorf-city?search=100-6365;dr-20240426-20240427;rc-1-2-10&optilens)
--- demo/inject.js
+++ demo/inject.js
@@ -7,6 +7,15 @@ var imageData = {
                 "https://cdn.pixabay.com/photo/2017/07/20/03/52/chicken-2521141_1280.png",
                 "https://cdn.pixabay.com/photo/2017/07/20/03/52/chicken-2521141_1280.png"
             ]
+        },
+        {
+            "id": 51291,
+            "heroImage": "https://cdn.pixabay.com/photo/2017/07/20/03/52/chicken-2521141_1280.png",
+            "galleryImages": [
+                "https://cdn.pixabay.com/photo/2024/03/15/22/12/ai-generated-8635876_1280.png",
+                "https://cdn.pixabay.com/photo/2024/03/15/22/12/ai-generated-8635876_1280.png",
+                "https://cdn.pixabay.com/photo/2024/03/15/22/12/ai-generated-8635876_1280.png"
+            ]
         }
     ]
 };
@@ -14,18 +23,12 @@ var imageData = {
 function alterImages() {
     clearTimeout(timer); 
 
-    fetch('https://raw.githubusercontent.com/json-iterator/test-data/master/large-file.json')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
-
     var accommodations = document.querySelectorAll("[data-testid='accommodation-list-element']");
     accommodations.forEach(function(accommodation) {
         var accommodationId = accommodation.getAttribute('data-accommodation');
 
         imageData.accommodations.forEach(imageData => {
             if (imageData.id == accommodationId) {
-                console.log('New image data for ' + accommodationId);
                 var mainImage = accommodation.querySelector("[data-testid='accommodation-main-image']");
                 mainImage.src = imageData.heroImage;
 
